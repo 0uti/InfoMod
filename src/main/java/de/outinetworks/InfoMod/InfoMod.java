6473097934147e0d8f577470e3e801e22c81beba
Outi <me@outi-networks.de>
2017-06-25 15:18:10 +0200

    updated version number

--- src/main/java/de/outinetworks/InfoMod/InfoMod.java
+++ src/main/java/de/outinetworks/InfoMod/InfoMod.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class InfoMod
 {
     public static final String MODID = "infomod";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.12.0";
     
     // The Keys used in the game.
 	public static KeyBinding SpawnOverlayKey;
